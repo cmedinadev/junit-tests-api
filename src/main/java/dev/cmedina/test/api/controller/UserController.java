9f7ba896d35f8abf973307cb77dc91cc03c25870
Cristian Medina <medina01@gmail.com>
2022-07-26 21:21:49 -0300

    adicionado o controller para User

---
+++ src/main/java/dev/cmedina/test/api/controller/UserController.java
@@ -0,0 +1,64 @@
+package dev.cmedina.test.api.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import dev.cmedina.test.api.domain.dto.UserDTO;
+import dev.cmedina.test.api.services.UserService;
+
+@RequestMapping(value = "/user")
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+	
+	@Autowired
+	private ModelMapper mapper;
+	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable("id") Integer id) {
+		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+	}
+	
+	@GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll()
+                .stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
+    }
+	
+	@PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj) {
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}").buildAndExpand(service.create(obj).getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj) {
+        obj.setId(id);
+        return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+	
+}
